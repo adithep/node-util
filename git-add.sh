afd5094a57743aa6b0d0d9a50f60d7d813cc541a
Adithep SriNarula <badsxx@gmail.com>
2014-07-25 22:54:16 +0700

    first

--- git-add.sh
+++ git-add.sh
@@ -1,21 +1,5 @@
-function go_on {
-  echo -ne "$1 [Y, n]\r"
-  read answer
-  if [ "$answer" = "n" ]; then
-    echo "exit"
-    exit 0
-  fi
-}
-
-function call {
-  go_on "$1"
-  $1
-  echo ""
-}
-
-call "cd ~/Desktop/website/packages/utilities"
-
-echo "Type commit message"
-read commit_message
-call "git add -A ."
-call "git commit -m $commit_message"
+#!/bin/bash
+cd ~/Desktop/website/packages/utilities
+git add -A .
+read -p "Commit description: " desc
+git commit -m "$desc"
