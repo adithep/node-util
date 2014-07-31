9cb01b4aee4d14c14078d4319b53158cdcc89f8e
Adithep <badsxx@gmail.com>
2014-07-31 19:46:25 +0700

    things

--- seed.js
+++ seed.js
@@ -32,18 +32,20 @@
 
   Seed.prototype.seed_all = function (database) {
     if (db_obj[database]) {
+      this.database = database;
       var server = new Server('127.0.0.1:27017');
       this.col = server.db(database).getCollection("data");
-      var s_json = EJSON.parse(fs.readFileSync('../json/essential-list-json/_s.json', 'utf-8'));
-      var keys_json = EJSON.parse(fs.readFileSync('../json/essential-list-json/keys.json', 'utf-8'));
+      var s_json = EJSON.parse(fs.readFileSync('../json/_s.json', 'utf-8'));
+      var keys_json = EJSON.parse(fs.readFileSync('../json/keys.json', 'utf-8'));
       var to_load = EJSON.parse(fs.readFileSync('../json/'+db_obj[database]+'/_s_to_load.json', 'utf-8'));
       if (s_json && keys_json && to_load) {
         var n_s_json = [];
         this.col.remove({});
-        var keys_obj = this.seed_keys(keys_json);
+        var key_s_obj = this.seed_schema(s_json, to_load);
+        var keys_obj = this.seed_keys(keys_json, key_s_obj, to_load);
         this.col.insert(keys_obj);
         //this.write_file(keys_obj, 'temp/keys.json');
-        this.seed_schema(s_json, to_load);
+
         this.check_all();
 
       }
@@ -100,25 +102,198 @@
   };
 
   Seed.prototype.value_check = function (key, value, schema, log) {
+    var log_obj, i, obj;
     if (key, value, schema) {
       switch(key.key_ty) {
-      case "_st":
-        if (key.key_arr) {
-          if(Array.isArray(value)) {
-            for(var i; i < value.length; i++) {
-              if (key.key_r && (this.col.find({_s_n: "_s", _s_n_for: key.key_s}).count()>=1)) {
-
-
-              } else {
+        case "_st":
+          if (key.key_arr) {
+            if(Array.isArray(value)) {
+              for(i = 0; i < value.length; i++) {
                 if ((typeof value === "string") && (String(value) !== "")) {
-                  return String(value);
+                  if (key.key_r) {
+                    if (key.key_s && key.key_key) {
+                      obj = {};
+                      obj._s_n = key.key_s;
+                      obj[key.key_key] = value;
+                      if (this.col.find(obj).count()===0) {
+                        log_obj = {
+                          schema: schema,
+                          key: key.key_n,
+                          log_ty: "cannot find r_value"
+                        };
+                        log.push(log_obj);
+                      }
+                    } else {
+                      log_obj = {
+                        schema: schema,
+                        key: key.key_n,
+                        log_ty: "no key_s or key_key"
+                      };
+                      log.push(log_obj);
+                    }
+
+                  }
+                } else {
+                  log_obj = {
+                    schema: schema,
+                    key: key.key_n,
+                    log_ty: "key_ty mismatched"
+                  };
+                  log.push(log_obj);
                 }
               }
+            } else {
+              log_obj = {
+                schema: schema,
+                key: key.key_n,
+                log_ty: "Value is not Array"
+              };
+              log.push(log_obj);
             }
           }
-        }
-        break;
-      default:
+          break;
+        case "_num":
+          if (key.key_arr) {
+            if(Array.isArray(value)) {
+              for(i = 0; i < value.length; i++) {
+                if ((typeof value === "number") && (isNaN(value) === false)) {
+                  if (key.key_r) {
+                    if (key.key_s && key.key_key) {
+                      obj = {};
+                      obj._s_n = key.key_s;
+                      obj[key.key_key] = value;
+                      if (this.col.find(obj).count()===0) {
+                        log_obj = {
+                          schema: schema,
+                          key: key.key_n,
+                          log_ty: "cannot find r_value"
+                        };
+                        log.push(log_obj);
+                      }
+                    } else {
+                      log_obj = {
+                        schema: schema,
+                        key: key.key_n,
+                        log_ty: "no key_s or key_key"
+                      };
+                      log.push(log_obj);
+                    }
+
+                  }
+                } else {
+                  log_obj = {
+                    schema: schema,
+                    key: key.key_n,
+                    log_ty: "key_ty mismatched"
+                  };
+                  log.push(log_obj);
+                }
+              }
+            } else {
+              log_obj = {
+                schema: schema,
+                key: key.key_n,
+                log_ty: "Value is not Array"
+              };
+              log.push(log_obj);
+            }
+          }
+          break;
+        case "_bl":
+          if (key.key_arr) {
+            if(Array.isArray(value)) {
+              for(i = 0; i < value.length; i++) {
+                if ((typeof value === "boolean")) {
+                  if (key.key_r) {
+                    if (key.key_s && key.key_key) {
+                      obj = {};
+                      obj._s_n = key.key_s;
+                      obj[key.key_key] = value;
+                      if (this.col.find(obj).count()===0) {
+                        log_obj = {
+                          schema: schema,
+                          key: key.key_n,
+                          log_ty: "cannot find r_value"
+                        };
+                        log.push(log_obj);
+                      }
+                    } else {
+                      log_obj = {
+                        schema: schema,
+                        key: key.key_n,
+                        log_ty: "no key_s or key_key"
+                      };
+                      log.push(log_obj);
+                    }
+
+                  }
+                } else {
+                  log_obj = {
+                    schema: schema,
+                    key: key.key_n,
+                    log_ty: "key_ty mismatched"
+                  };
+                  log.push(log_obj);
+                }
+              }
+            } else {
+              log_obj = {
+                schema: schema,
+                key: key.key_n,
+                log_ty: "Value is not Array"
+              };
+              log.push(log_obj);
+            }
+          }
+          break;
+        case "currency":
+          if (key.key_arr) {
+            if(Array.isArray(value)) {
+              for(i = 0; i < value.length; i++) {
+                if ((typeof value === "number") && (isNaN(value) === false)) {
+                  if (key.key_r) {
+                    if (key.key_s && key.key_key) {
+                      obj = {};
+                      obj._s_n = key.key_s;
+                      obj[key.key_key] = value;
+                      if (this.col.find(obj).count()===0) {
+                        log_obj = {
+                          schema: schema,
+                          key: key.key_n,
+                          log_ty: "cannot find r_value"
+                        };
+                        log.push(log_obj);
+                      }
+                    } else {
+                      log_obj = {
+                        schema: schema,
+                        key: key.key_n,
+                        log_ty: "no key_s or key_key"
+                      };
+                      log.push(log_obj);
+                    }
+
+                  }
+                } else {
+                  log_obj = {
+                    schema: schema,
+                    key: key.key_n,
+                    log_ty: "key_ty mismatched"
+                  };
+                  log.push(log_obj);
+                }
+              }
+            } else {
+              log_obj = {
+                schema: schema,
+                key: key.key_n,
+                log_ty: "Value is not Array"
+              };
+              log.push(log_obj);
+            }
+          }
+          break;
+        default:
       }
     }
 
@@ -133,7 +308,7 @@
         for(var doc_k in doc) {
           if ((s_n._s_keys.indexOf(doc_k) !== -1)) {
             var key_obj = keys[doc_k];
-            self.value_check(key_obj, doc[doc_k], s_n._s_n_for, log);
+            //self.value_check(keys[doc_k], doc[doc_k], s_n._s_n_for, log);
           } else {
             var log_obj = {
               key: doc_k,
@@ -186,12 +361,12 @@
   };
 
   Seed.prototype.seed_s_json_tri = function (arr, filter, s_n) {
-    var tri_defaults = EJSON.parse(fs.readFileSync('../json/marathon-str-json/_tri_defaults.json', 'utf-8'));
+    var tri_defaults = EJSON.parse(fs.readFileSync('../json/'+db_obj[this.database]+'/_tri_defaults.json', 'utf-8'));
     for(var i = 0; i < arr.length; i++) {
       if (filter.indexOf(arr[i]._n) === -1) {
         arr.splice(i, 1);
       } else {
-        var json_path = '../json/' + arr[i].path;
+        var json_path = '../json/'+db_obj[this.database]+'/' + arr[i].path;
         var json = EJSON.parse(fs.readFileSync(json_path, 'utf-8'));
         for(var m = 0; m < json.length; m++) {
           if (tri_defaults[json[m]._tri_ty]) {
@@ -207,12 +382,18 @@
 
   };
 
-  Seed.prototype.seed_s_json = function (arr, filter, s_n) {
+  Seed.prototype.seed_s_json = function (arr, filter, s_n, opt) {
+    var json_path;
     for(var i = 0; i < arr.length; i++) {
       if (filter.indexOf(arr[i]._n) === -1) {
         arr.splice(i, 1);
       } else {
-        var json_path = '../json/' + arr[i].path;
+        if (opt) {
+          json_path = '../json/'+db_obj[this.database]+'/' + arr[i].path;
+        } else {
+          json_path = '../json/' + arr[i].path;
+        }
+
         var json = EJSON.parse(fs.readFileSync(json_path, 'utf-8'));
         for(var m = 0; m < json.length; m++) {
           this.create_obj(json[m], s_n);
@@ -222,25 +403,32 @@
     }
 
   };
-
   Seed.prototype.seed_schema = function (s_json, to_load) {
+    var key_s_obj = [];
     var n_s_json = [];
     for(var i = 0; i < s_json.length; i++) {
       if (to_load[s_json[i]._s_n_for]) {
         if (s_json[i]._s_n_for === "_tri") {
           this.seed_s_json_tri(s_json[i].json, to_load[s_json[i]._s_n_for], s_json[i]._s_n_for);
+        } else if (s_json[i].web_spec) {
+          this.seed_s_json(s_json[i].json, to_load[s_json[i]._s_n_for], s_json[i]._s_n_for, true);
         } else {
           this.seed_s_json(s_json[i].json, to_load[s_json[i]._s_n_for], s_json[i]._s_n_for);
         }
         this.create_obj(s_json[i]);
+        key_s_obj = _.union(key_s_obj, s_json[i]._s_keys);
         n_s_json.push(s_json[i]);
       } else if ((s_json[i]._s_n_for === "_s") || (s_json[i]._s_n_for === "keys")) {
         this.create_obj(s_json[i]);
+        key_s_obj = _.union(key_s_obj, s_json[i]._s_keys);
         n_s_json.push(s_json[i]);
       }
 
+
     }
+
     this.col.insert(n_s_json);
+    return key_s_obj;
   };
 
   Seed.prototype.write_file = function (obj, path_to) {
@@ -261,25 +449,39 @@
 
   };
 
-  Seed.prototype.seed_keys = function (keys_json) {
+  Seed.prototype.seed_keys = function (keys_json, key_s_obj, to_load) {
     if (keys_json && Array.isArray(keys_json)) {
       var arr = [];
       for(var i = 0; i < keys_json.length; i++) {
-        keys_json[i]._s_n = "keys";
-        keys_json[i]._usr = "root";
-        if (keys_json[i].key_arr === true) {
-          var obj = this.create_key_arr(keys_json[i]);
-          arr.push(obj);
-          delete keys_json[i].key_arr;
-        }
-        keys_json[i]._dt = new Date();
-        if (!keys_json[i]._id) {
-          keys_json[i]._id = random.id();
-        }
+
+          keys_json[i]._s_n = "keys";
+          keys_json[i]._usr = "root";
+
+          if (keys_json[i].key_r === true) {
+            if (!to_load[keys_json[i].key_s]) {
+              delete keys_json[i].key_r;
+              delete keys_json[i].key_s;
+              delete keys_json[i].key_key;
+            }
+          }
+          if (keys_json[i].key_arr === true) {
+            var obj = this.create_key_arr(keys_json[i]);
+            if (key_s_obj.indexOf(obj.key_n) !== -1 ) {
+              arr.push(obj);
+            }
+            delete keys_json[i].key_arr;
+          }
+          keys_json[i]._dt = new Date();
+          if (!keys_json[i]._id) {
+            keys_json[i]._id = random.id();
+          }
+          if (key_s_obj.indexOf(keys_json[i].key_n) !== -1 ) {
+            arr.push(keys_json[i]);
+          }
+
 
       }
-      var json = keys_json.concat(arr);
-      return json;
+      return arr;
     }
 
   };
